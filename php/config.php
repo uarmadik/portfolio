5ef920c3b22a40054f75c180f3036e30c3f6ec71
uarmadik <uarmadik@gmail.com>
2017-08-17 11:44:26 +0300

    Modified sending email from portfolio web site

--- php/config.php
+++ php/config.php
@@ -3,4 +3,6 @@
     $config = ['from'    => 'Portfolio_Ihor_Harmider@userok.zzz.com.ua',
                'to'      => 'uarmadik@gmail.com',
                'subject' => 'Message from Portfolio web site',
-               'headers' => 'From: Portfolio_Ihor_Harmider@userok.zzz.com.ua'];
+               'headers' => 'Content-type: text/html; charset=utf-8' . "\r\n" .
+                            'MIME-Version: 1.0' . "\r\n" .
+                            'From: Portfolio_Ihor_Harmider@userok.zzz.com.ua '];
--- php/mail.php
+++ php/mail.php
@@ -12,6 +12,18 @@
     $message_from_user = $mail->validate_text($_POST['message']);
 
     if (($user_email) && ($user_name) && ($message_from_user)){
-        $message = $message_from_user . "\n \n From: " . $user_name . ", email: " . $user_email;
+        $message = '<html>
+                        <head>
+                          <title>Message from Portfolio web site</title>
+                        </head>
+                        <body>
+                          <img src="http://userok.zzz.com.ua/portfolio/img/portfolio-min.jpg" title="portfolio picture" 
+                          alt="portfolio" width="250"
+                          style="margin: 0 auto"/>
+                          <p style="padding: 0.5em; font: normal 1.2em sans-serif; line-height: 1.3em ;color: #444;">'. $message_from_user .'</p>
+                          <p style="font: bold 1.2em sans-serif">From: '. $user_name . ' </p>
+                          <p style="font: bold 1.2em sans-serif">Email: ' . $user_email . '</p>
+                        </body>
+                    </html>';
         $mail->send($message);
     }
